066c3a546a4dea81fd375b3a2b6eaa9075e1f058
GARTNER\sujha <sumit.jha@gartner.com>
2020-04-08 23:32:20 +0530

    Updated

--- GifPlayer/app/src/main/java/com/viceboy/babble/ui/screens/forgotPassword/ForgotPasswordViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.viceboy.babble.ui.screens.forgotPassword
-
-import com.viceboy.babble.ui.base.BaseViewModel
-
-class ForgotPasswordViewModel : BaseViewModel<Boolean>() {
-
-}
--- GifPlayer/data_repo/src/main/java/com/viceboy/data_repo/util/DataConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.viceboy.data_repo.util
-
-object DataConstants {
-    const val KEY_AMOUNT = "amount"
-}
